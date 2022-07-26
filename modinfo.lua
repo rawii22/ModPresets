f3a21b62b4a7cfa189f72ecbe4e376a5115da345
Ricky <rjromanach@gmail.com>
2022-07-26 07:16:05 -0500

    Fix modinfo, improved modicon and preview
    I forgot to uncomment the modicon definitions in modinfo.

--- modinfo.lua
+++ modinfo.lua
@@ -3,8 +3,8 @@ author = "rawii22 & lord_of_les_ralph"
 description = "Allows you to store and apply mod presets for server mods (for now). The preset system works exactly the same as the world settings presets."
 version = "1.0"
 version_compatible = "1.0"
---icon = "modicon.tex"
---icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+icon_atlas = "modicon.xml"
 forumthread = ""
 
 dst_compatible = true
